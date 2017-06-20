a40d487fe17fd4ac4f66affe01c579da82f53e9b
Ethan Tran <ethandt92@gmail.com>
2017-06-19 23:03:30 -0500

    refactor random utils

--- src/randomuser.js
+++ src/randomuser.js
@@ -1,13 +1,8 @@
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function getRandomBoolean() {
-    return Math.random() >= 0.5
-}
+import randomboolean from './randomboolean'
+import randomnumber from './randomnumber'
 
 export default function (size = 'thumb') {
-    const gender = getRandomBoolean() ? 'men': 'women'
-    const number = Math.floor(getRandomArbitrary(0, 100))
+    const gender = randomboolean() ? 'men': 'women'
+    const number = Math.floor(randomnumber(0, 100))
     return `https://randomuser.me/api/portraits/${size}/${gender}/${number}.jpg`
 }
